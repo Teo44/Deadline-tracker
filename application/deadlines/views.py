6b70e05198eb5a0ce57f07f4b1b179fa1ac4e6a4
Teo44 <43036149+Teo44@users.noreply.github.com>
2019-03-18 18:30:25 +0200

    functionality to mark deadlines as completed

--- application/deadlines/views.py
+++ application/deadlines/views.py
@@ -2,6 +2,7 @@ from application import app, db
 from flask import render_template, request, redirect, url_for
 from application.deadlines.models import Deadline
 
+# /deadlines list all the deadlines from the database
 @app.route("/deadlines", methods=["GET"])
 def deadlines_index():
     return render_template("deadlines/list.html", deadlines = Deadline.query.all())
@@ -16,6 +17,15 @@ def deadlines_create():
     d = Deadline(request.form.get("name"))
 
     db.session().add(d)
+    db.session().commit()
+
+    return redirect(url_for("deadlines_index"))
+
+@app.route("/deadlines/<deadline_id>/", methods=["POST"])
+def set_deadline_done(deadline_id):
+
+    d = Deadline.query.get(deadline_id)
+    d.done = True
     db.session().commit()
 
     return redirect(url_for("deadlines_index"))
